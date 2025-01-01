26620ea4ac34914bd7833bb9e8f3e4542cbe4588
NaikVedhas <vedhasnaik121@gmail.com>
2025-01-01 14:45:33 +0530

    Done

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator =  require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -14,4 +16,41 @@ const  userSchema = new Schema({
     }
 });
 
+
+//Static signup methodwe can add the signup logic in userController too but we will do it here by attaching a method to model
+
+
+userSchema.statics.signup = async function (email,password) { //when we use this keyword we cant use arriw function
+    
+    //validation
+
+    if(!email || !password){
+        throw Error("Email and Pawword is required");
+    }
+    
+    if(!validator.isEmail(email)){
+        throw Error("Email is not valid");
+    }
+    
+    if(!validator.isStrongPassword(password)){
+        throw Error("Password is not strong enough");
+    }
+    
+    //First well check if any email already exists or not
+
+    const exists = await this.findOne({email});         //here we dont have acess to User (ie User.find or any other functions) becase in this file down we are exporting it only na so we have this which store the current model
+
+    if(exists) { //when user exists na then that is stored in exists otherwise null
+        throw Error("User already exist");
+    }
+
+    //well store the password by hashing them using bcrpt. Also before hashing it adds a random string to our password for extra security called salt. 
+    
+    const salt = await bcrypt.genSalt(10);//we add a number jitna bada number hoga utna time lagega hacker ko password crack karne mein par utna hi time lagega users ko signup karne mein so we need to find a balance
+    const hash = await bcrypt.hash(password,salt);
+
+    const user = await this.create({email,password:hash});
+
+    return user;  // we will call from function from diff location so we are returning  
+}
 module.exports = mongoose.model('User',userSchema);
